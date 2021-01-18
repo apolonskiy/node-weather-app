1054a968c4867f0b62b9c9e4c1f303395b3792b7
apolonskyi <apolonskyi@alteryx.com>
2021-01-18 14:22:45 +0200

    remove console

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client file JS file loaded')
-
 const form = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
